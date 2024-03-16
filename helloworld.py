e28dd057241915c6dfe583c1dd40beb2685ac179
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2024-03-16 09:37:30 +0530

    hello world program

--- helloworld.py
+++ helloworld.py
@@ -1,21 +1,22 @@
-print("hello world")
-x = 'hello'
-print(type(x))
-y = "SivaLingam"
-b = len(y)
-print(b)
-y = "SivaLingam"
-b = y.replace("lingam2","sivalingam")
-print(b)
-# split
-c = "sivalingam devops engineer"
-d = c.split(" ")
-print(d)
-a = "great"
-b = "doblegraete"
-c = a + " " + b
-print(c)
+# print("hello world")
+# x = 'hello'
+# print(type(x))
+# y = "SivaLingam"
+# b = len(y)
+# print(b)
+# y = "SivaLingam"
+# b = y.replace("lingam2","sivalingam")
+# print(b)
+# # split
+# c = "sivalingam devops engineer"
+# d = c.split(" ")
+# print(d)
+# a = "great"
+# b = "doblegraete"
+# c = a + " " + b
+# print(c)
 
 a = "python is awesome language"
-b = a.replace("python", "java")
-print("modified text is", b)
+b = a[:4]
+print(b)
+
