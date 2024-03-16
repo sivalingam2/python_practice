9689becd245692b8331e5f61dd283b2ba57dc8fa
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2024-03-16 09:44:40 +0530

    hello world program

--- helloworld.py
+++ helloworld.py
@@ -8,9 +8,9 @@
 # b = y.replace("lingam2","sivalingam")
 # print(b)
 # # split
-# c = "sivalingam devops engineer"
-# d = c.split(" ")
-# print(d)
+c = "sivalingam devops engineer"
+d = c.split(" ")
+print(d[1])
 # a = "great"
 # b = "doblegraete"
 # c = a + " " + b
